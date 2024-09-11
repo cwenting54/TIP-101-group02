58bc70e97f14b9690d920d96064b213a938233a9
Sophie <cwenting54@gmail.com>
2024-09-11 13:05:09 +0800

    test

--- src/APP/Sophie.java
+++ src/APP/Sophie.java
@@ -3,6 +3,6 @@ package APP;
 public class Sophie {
 	public static void main(String[] args) {
 		System.out.println("hi hi hi");
-		System.out.println("test branch");
+		System.out.println("test");
 	}
 }
